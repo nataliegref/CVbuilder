85e467113c4c79d66708424f39530ab3c593feeb
Natalie Grefenstette <nataliegref@gmail.com>
2023-09-21 20:57:39 +0200

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+    <h1>Welcome to your custom CV builder</h1>
+    </>
+  )
+}
+
+export function CV() {
+
+  return (
+    <>
+    <h2>CV</h2>
+    </>
+  )
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App, CV} from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <CV />
+  </React.StrictMode>,
+)
