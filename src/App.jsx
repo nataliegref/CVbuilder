e4fa738b00cccc90f64dcb97d526d5f28878133a
Natalie Grefenstette <nataliegref@gmail.com>
2023-09-22 11:53:16 +0200

    Added a name field that can be submitted and edited

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,53 @@ export function App() {
     </>
   )
 }
+// eslint-disable-next-line react/prop-types
+function FormItem({name, setName, title}){
+
+  const [formVisible, setFormVisible] = useState(true);
+  const [formValue, setFormValue] = useState('')
+
+  const handleSubmit = (event) => {event.preventDefault();
+    setFormVisible(false)
+  }
+  const handleEdit = (event) => {event.preventDefault();
+    setFormVisible(true)
+    setFormValue(name)
+  }
+
+  return (<>
+  <form onSubmit = {handleSubmit}>
+  <span className = 'item'>{title} </span>  
+
+{  formVisible &&  (
+  <input type="text"
+  value = {formValue}
+    onChange={(event) => {
+      setName(event.target.value)
+    setFormValue(event.target.value)
+  }}
+    /> )}
+
+</form>
+
+{  formVisible ?(<button onClick={
+  handleSubmit}>Submit</button>)
+:(<button onClick={
+    handleEdit}>Edit</button>)
+    }
+</>)
+}
+
 
 export function CV() {
-
+  const [name, setName] = useState('')
   return (
     <>
     <h2>CV</h2>
+    <FormItem name = {name} setName = {setName} title={'Full Name'}/>
+    <div id='overview'>
+    <div id='name'>{name}</div>
+    </div>
     </>
   )
 }
