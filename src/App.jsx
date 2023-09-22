bb4c02e387ead485c34d3366ce326f52e08a4cf8
Natalie Grefenstette <nataliegref@gmail.com>
2023-09-22 13:36:15 +0200

    Added fields and make the input forms flexible in width and size

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export function App() {
@@ -9,18 +9,26 @@ export function App() {
     </>
   )
 }
+
 // eslint-disable-next-line react/prop-types
-function FormItem({name, setName, title}){
+function FormItem({item, setItem, title, row=1, col=15}){
 
   const [formVisible, setFormVisible] = useState(true);
   const [formValue, setFormValue] = useState('')
+  const [textAreaWidth, setTextAreaWidth] = useState(col)
+
+  useEffect(() => {
+    const maxWidth = 40; 
+    const width = Math.max(col, formValue.length)
+    setTextAreaWidth(Math.min(maxWidth, width));
+  }, [formValue, col]);
 
   const handleSubmit = (event) => {event.preventDefault();
     setFormVisible(false)
   }
   const handleEdit = (event) => {event.preventDefault();
     setFormVisible(true)
-    setFormValue(name)
+    setFormValue(item)
   }
 
   return (<>
@@ -28,34 +36,65 @@ function FormItem({name, setName, title}){
   <span className = 'item'>{title} </span>  
 
 {  formVisible &&  (
-  <input type="text"
+  <textarea rows={row} 
+  cols={textAreaWidth}
   value = {formValue}
     onChange={(event) => {
-      setName(event.target.value)
+      setItem(event.target.value)
     setFormValue(event.target.value)
   }}
-    /> )}
+    ></textarea> 
+    )}
+
 
 </form>
 
-{  formVisible ?(<button onClick={
-  handleSubmit}>Submit</button>)
-:(<button onClick={
-    handleEdit}>Edit</button>)
+{  formVisible ?(<>
+<button onClick={handleSubmit}>Submit</button>
+<button disabled={true} className="disabled-button" onClick={
+handleEdit}>Edit</button>
+</>
+  )
+:(<>
+  <button disabled={true} className="disabled-button" onClick={handleSubmit}>Submit</button>
+  <button onClick={
+  handleEdit}>Edit</button>
+  </>
+    )
     }
-</>)
+  
+</>
+)
 }
 
 
-export function CV() {
-  const [name, setName] = useState('')
+
+
+export function Overview() {
+  const [name, setName] = useState('Jane Smith')
+  const [email, setEmail] = useState('jane@gmail.com')
+  const [phoneNumber, setPhoneNumber] = useState('+99 999 9999')
+  const [exp1, setExp1] = useState('')
   return (
     <>
     <h2>CV</h2>
-    <FormItem name = {name} setName = {setName} title={'Full Name'}/>
+    <FormItem item = {name} setItem = {setName} title={'Full name'}/>
+    <FormItem item = {email} setItem = {setEmail} title={'Email address'}/>
+    <FormItem item = {phoneNumber} setItem = {setPhoneNumber} title={'Phone number'}/>
+    
+    <FormItem item = {exp1} setItem = {setExp1} title={'Experience'} row ={8} col = {50}/>
     <div id='overview'>
+      <h2>Overview</h2>
     <div id='name'>{name}</div>
+    <div id='email'>{email}</div>
+    <div id='phone'>{phoneNumber}</div>
     </div>
+
+    <div id='Experience'>
+    <h2>Education</h2>
+    <div id='exp1'>{exp1}</div>
+    </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App, CV} from './App.jsx'
+import {App, Overview} from './App.jsx'
+// import {WorkCV} from './Work.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <CV />
+    <Overview />
   </React.StrictMode>,
 )
